5aede1bef7c8466dd5c2ce24b15e4937314b5dfc
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2024-08-27 17:45:01 +0200

    Update updateStationLogo.js

--- StationLogo/updateStationLogo.js
+++ StationLogo/updateStationLogo.js
@@ -12,7 +12,7 @@
 
 ///  This plugin only works from web server version 1.2.6!!!
 
-const enableSearchLocal = false; // Enable or disable searching local paths (.../web/logos)
+const enableSearchLocal = true; // Enable or disable searching local paths (.../web/logos)
 const enableOnlineradioboxSearch = true; // Enable or disable onlineradiobox search if no local or server logo is found.
 const updateLogoOnPiCodeChange = true; // Enable or disable updating the logo when the PI code changes on the current frequency. For Airspy and other SDR receivers, this function should be set to false.
 
