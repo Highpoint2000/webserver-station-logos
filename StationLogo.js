31f2c4730171f6bb25c03c673d7f636d69c52310
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2024-11-19 20:09:28 +0100

    Update StationLogo.js

--- StationLogo.js
+++ StationLogo.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'Station Logo Plugin',
-    version: '3.4b',
+    version: '3.4c',
     author: 'Highpoint',
     frontEndPath: 'StationLogo/updateStationLogo.js'
 }
